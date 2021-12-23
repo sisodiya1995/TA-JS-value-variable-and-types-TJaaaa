37cbfee2b69fa1372f7a4cf558c6bf7df2824916
Akash Singh <akashingh95@gmail.com>
2021-12-23 19:46:39 +0530

    truthy-falsy-block-3

--- block-BJaaao/code/index.js
+++ block-BJaaao/code/index.js
@@ -16,6 +16,10 @@ let num2 = 32;
 - Alert `The value of num1 is [num1] and the value of num2 is [num2]`
 - Alert `The sum of [num1] and [num2] is [num1 + num1]`
 */
+    let language = "javaScript";
+    alert(`I am learning ${language}`);
+    alert(`The value of num1 is ${num1} and the value of num2  ${num2}`);
+    alert(`The sum of ${num1} and  ${num2}  is  ${num1 +num2}`);
 
 /*
 2. Do the following using `alert`, `prompt` and `confirm` and `console`
@@ -31,7 +35,17 @@ let num2 = 32;
   - Log the message using console.log `Username: [userName]
     Adult: [isAdult]
     Profession: [userProfession]` (it should be in three different lines)
-*/
+*/let userName = prompt("name of the user");
+ let  userProfession =prompt("profession of the user");
+ let isAdult = confirm(" user is adult");
+ alert(`I am ${userName} a ${userProfession}`);
+ console.log(`I am ${userName} a ${userProfession} .`);
+ alert(`UserName : ${userName}
+ Adult : ${isAdult}
+ Profession :${userProfession}`);
+ console.log(`UserName : ${userName} 
+  Adult : ${isAdult}
+  Profession : ${userProfession} `)
 
 /*
 . Addition using prompt
@@ -43,3 +57,10 @@ let num2 = 32;
   - Log using console.log [numA - numB]
   - Log using console.log [numA * numB]
 */
+ let numA = prompt("numA");
+ let numB = prompt("numB");
+  alert(`the sum of both numbers .${Number(numA) + Number(numB)}`);
+  alert(`numA - numB = ${numA -numB} `);
+  alert(`numA * numB = ${numA * numB} `);
+  console.log(`numA-numB =${numA - numB}`);
+  console.log(`numA *numB =${numA * numB}`);
