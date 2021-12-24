48087831546b813bf665969f4ed17a1c4cb58d4b
Akash Singh <akashingh95@gmail.com>
2021-12-24 20:52:12 +0530

    Assign-level-2|block2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -5,26 +5,67 @@
 3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
+   let user =prompt("user name");
+   alert(user);
+    userName = user;
+   console.log(user);
+   console.log(userName);
+   let age , isLearning;
+    age = 22;
+    isLearning =true;
+    user ="john"
+    console.log(`${user}  ${userName}`); 
 
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+  let numA =prompt("numA");
+   let numB =prompt("numB");
+   let sum = Number(numA) +Number(numB);
+   alert(`sum of numA$ {numA} and numB ${numB} is equal to ${sum}`);
+ 
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+  for (let j = 0; j <= 10; j++) {
+     if(j % 2 == 0){
+        console.log(j);
+     }
+    }  
+
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
-// 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+  for(let i=10; i>=1;  i--){
+     if( i % 2 !==0){
+         console.log(i);
+     }
+ }   
 
+// 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+  let j=5;
+sum ="";
+while(j >= 0){
+    sum = sum + String(j);
+    j--;
+}
+console.log(sum); 
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
 
+let j=10;
+sum ="";
+while(j >= 0){
+    if(j % 2 == 0)
+    sum = sum + String(j);
+    j--;
+}
+console.log(sum);
 // Comparisoin
 
 /* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
+
 
 - `alert` true or false base on the entered value.
 - if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
@@ -36,3 +77,14 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = prompt("num1");
+let num2 = prompt("num2");
+ if(num1 && num2 =="true"){
+     alert("Enter a valid value");
+ }else if(num1 && num2 =="null"){
+    alert("Enter a valid value");
+ } else if(num1 && num2 =="undefined"){
+    alert("Enter a valid value");
+ }
+alert(`${num1 === num2}`);
