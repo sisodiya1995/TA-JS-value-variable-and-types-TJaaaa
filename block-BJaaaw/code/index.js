a5819f738735e95efa5e6129c0d5a97afdc5c89c
Akash Singh <akashingh95@gmail.com>
2021-12-24 20:41:32 +0530

    loop-block-2

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -6,10 +6,25 @@
   - `if` the number is even print the message " number is even"
   - `if` the number is odd print the message "number is odd"
 */
-
+ let user = 13;
+ if(user % 2 == 0){
+   alert('number is even');
+ }else{
+   alert('number is odd');
+ }
 // 2. Write a program to accept two numbers from user using`prompt` and alert the max value.
+let num1 = prompt('enter the first number');
+let num2 = prompt('enter the second number');
+ if(Number(num1)  > Number(num2)){
+ alert(`max value ${num1}`);
+}else{
+  alert(`max value ${num2}`);
+} 
+
+
 
 // 3. Convert the above code using`?` terniary operator
+Number(num1)  > Number(num2) ?  alert(`max value ${num1}`) :alert(`max value ${num2}`);
 
 /*
 4. Write a program that asks the user for the house name and check the following conditions:
@@ -18,12 +33,72 @@
   - `if` house name is "lannister" then print the message " A lannister always pays his debt"
   - `else` print the message " All men must die"
 */
+let houseName = "stark";
+if(houseName == "stark"){
+  alert("Winter is coming");
+}else if(houseName == "lannister"){
+  alert(" A lannister always pays his debt");
+}else{
+  alert(" All men must die");
+}
+
 
 // 5. Convert the above code using`?` terniary operator
+houseName == "stark" ?  alert("Winter is coming"): houseName == "lannister" ? alert(" A lannister always pays his debt") :alert(" All men must die");
 
 // Switch
 
 // 6. Write a program that takes the number of the month(1 - 12) and alert number of days in the month.
+let month = 4;
+switch(month){
+  case 1:
+    alert("day31")
+    break;
+
+  case 2:
+   alert("day-28")
+   break;
+  case 3:
+    alert("day -31")
+    break;
+
+  case 4:
+   alert("day -30")
+   break;
+
+  case 5:
+   alert("day -31")
+   break;
+
+  case 6:
+  alert("day -30")
+  break;
+   
+  case 7:
+  alert("day -31")
+  break;
+
+  case 8:
+  alert("day -31")
+  break;
+
+  case 9:
+  alert("day -31")
+  break; 
+
+  case 10:
+    alert("day -31")
+    break;
+
+  case 11:
+  alert("day -31")
+  break;
+
+  case 12:
+  alert("day -31")
+  break;
+           
+}
 
 /* 7.
 - Write a program that take the salery of the user using prompt and alert the in -hand amount.You will find out the in -hand amount by deducting the tax amoun from salery.Conditions are given below.
@@ -32,6 +107,14 @@
   - `Salary > 50000` tax is 30 %
 
 */
+let Salary = prompt("Enter your salary");
+if(Number(Salary)<= 20000){
+   alert(`in -hand salary =${Number(Salary)- Number(Salary)*10/100}`);
+}else if(Number(Salary)<= 40000 ){
+  alert(`in -hand salary =${Number(Salary)- Number(Salary)*20/100}`);
+}else{
+  alert(`in -hand salary =${Number(Salary)- Number(Salary)*50/100}`);
+}
 
 //  if..else vs switch
 
@@ -45,6 +128,39 @@ Implement the condition give below using`if..else` and`switch` statement.
   - `marks > 0` alert`"Grade D"`
 
 */
+/*
+let marks =80;
+if(marks > 100){
+  alert("Marks can't be greater than 100");
+}else if(marks > 80 && marks < 100){
+  alert("Grade A");
+}else if (marks > 50 && marks < 80){
+  alert("Grade B");
+}else if(marks > 30 && marks < 50){
+  alert("Grade C");
+}else if(marks > 0){
+  alert("Grade D");
+}else{
+  alert("fail")
+}*/
+let marks = 50;
+switch(true){
+       case(marks > 100):
+        alert("Marks can't be greater than 100");  
+        break;
+        case(marks > 80 && marks < 100):
+          alert("Grade A");  
+          break;
+          case(marks > 50 && marks < 80):
+            alert("Grade B"); 
+            break;
+            case(marks > 30 && marks < 50):
+              alert("Grade C"); 
+              break;
+              case(marks > 0):
+                alert("Grade D"); 
+                break;
+}
 
 /* 9. Weather app
 
@@ -55,3 +171,35 @@ Implement the condition give below using`if..else` and`switch` statement.
   - If`freezing` alert`Get your sweeter on`
   - Anything else should alert`Not a valid input`
 */
+
+
+
+let userSay ="sunny";
+if(userSay =="sunny"){
+   alert(`Wear a T-shirt`);
+}else if (userSay =="rainy"){
+  alert(`Don't forget to take your raincoat`)
+}else if(userSay =="hot" ){
+   alert(`Get a hanky`);
+}else if(userSay =="frezing"){
+   alert(`Get your sweeter on`);
+}else{
+  alert(`Not a valid input`);
+} 
+
+
+let userSay ="frezing";
+ switch(true){
+       case (userSay == "sunny"):
+         alert(`Wear a T-shirt`);
+         break;
+         case(userSay =="rainy"):
+          alert(`Don't forget to take your raincoat`);
+          break;  
+          case(userSay =="hot"):
+          alert(`get a hankey`);
+          break;  
+          case(userSay =="frezing"):
+          alert(`get your swetter on`);
+          break;      
+ }
